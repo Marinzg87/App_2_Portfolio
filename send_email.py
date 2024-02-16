4af00519b5cda47d60b2a475907307d073370a73
Marcin Bogusz <bogusz.marcin@icloud.com>
2024-02-16 08:18:08 +0100

    Send emails using the form.

--- send_email.py
+++ send_email.py
@@ -1,19 +1,14 @@
 import smtplib
 
-host = "smtp.gmail.com"
-port = 465
 
-user_name = "m.bogusz.zg@gmail.com"
-password = "wxphabpbyfbnhnrf"
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
 
-receiver = "m.bogusz.zg@gmail.com"
+    user_name = "m.bogusz.zg@gmail.com"
+    password = "wxphabpbyfbnhnrf"
+    receiver = "m.bogusz.zg@gmail.com"
 
-message = """\
-Subject: Hi!
-How are you?
-Bye!
-"""
-
-with smtplib.SMTP_SSL(host, port) as server:
-    server.login(user_name, password)
-    server.sendmail(user_name, receiver, message)
+    with smtplib.SMTP_SSL(host, port) as server:
+        server.login(user_name, password)
+        server.sendmail(user_name, receiver, message)
